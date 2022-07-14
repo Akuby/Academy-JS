7c5baa02e78e58c059b8ea3a3fe4f0fa40265491
Akuby <akuby1997@gmail.com>
2022-07-14 13:52:03 +0900

    minor changes

--- 12. 실습/navigation/common2.js
+++ 12. 실습/navigation/common2.js
@@ -1,9 +1,13 @@
 const gnb = document.getElementById('gnb')
 const headerWrap = document.getElementById('headerWrap')
 
+let allMenuView = (h) => { // 익명함수는 선언 전 호출 불가
+  headerWrap.style.height = `${h}px`
+}
+
 gnb.addEventListener('mouseenter', () => {
-  headerWrap.style.height = '300px';
+  allMenuView(300)
 })
 headerWrap.addEventListener('mouseleave', () => {
-  headerWrap.style.height = '80px';
+  allMenuView(80)
 })
